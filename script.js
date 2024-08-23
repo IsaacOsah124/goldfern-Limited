8e424f1916950f4737239d69e073e8d305280086
IsaacOsah124 <isaacosah124@gmail.com>
2024-08-23 08:56:52 +0000

    Develop the Backgound image section to the view card section

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const toggleButton = document.querySelector(".toggle-button");
-const navbarLinks = document.querySelector(".navbar-links");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
